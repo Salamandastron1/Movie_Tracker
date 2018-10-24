566f936022a90fd23e56253b54621e786cf92bd0
Intrepid Panda <jerick999@gmail.com>
2018-10-24 14:56:46 -0600

    Set up standard it blocks in API describe block

---
+++ src/util/Helper.test.js
@@ -0,0 +1,18 @@
+import * as API from './Helper.js';
+import * as mock from './mocks.js';
+
+describe('API', () => {
+  describe('getMovieData', () => {
+    it('should call fetch with the correct params', async () => {
+
+    });
+
+    it('should return data in the correct format', async () => {
+
+    });
+
+    it('should throw an error if the status is not ok', async () => {
+
+    });
+  });
+});

b89cae32215ac5a6f9ef8a9eb3e8e235a12b6428
Justin Stewart <Justinstewart3313@gmail.com>
2018-10-26 17:06:36 -0600

    Complete iteration0

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const Card = ({ title , poster_path , overview , release_date , favorited }) => {
+const Card = ({ movie }) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <img src={'https://image.tmdb.org/t/p/w300_and_h450_bestv2/' + poster_path} />
-      <p>{overview}</p>
-      <p>{release_date}</p>
+      <h1>{movie.title}</h1>
+      <img src={'https://image.tmdb.org/t/p/w300_and_h450_bestv2/' + movie.poster_path} />
+      <p>{movie.overview}</p>
+      <p>{movie.release_date}</p>
     </div>
   )
 }

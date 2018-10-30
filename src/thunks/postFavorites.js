b600fb328d2e1ca7166e67d5e967686edc64d7ff
Tim Garrity <tag.garrity@gmail.com>
2018-10-30 11:11:47 -0600

    Add in action creators to thunk with dispatch

--- src/thunks/postFavorites.js
+++ src/thunks/postFavorites.js
@@ -2,7 +2,13 @@ import { toggleFavorite } from '../Action-creators/toggleFavorite';
 import { errorReceived } from '../Action-creators/errorReceived';
 
 export const postFavorites = (userId, movie) => {
-  return dispatch => {
-    dispatch(toggleFavorite(movie.id));
+  const url = 'http://localhost:3000/api/users/favorites/new';
+  return async (dispatch) => {
+    const response = await fetch(url)
+    if(response.ok) {
+      dispatch(toggleFavorite(movie.id));
+    } else {
+      dispatch(errorReceived('Favorite not added, 500 error'))
+    }
   }
 }
--- src/thunks/postFavorites.test.js
+++ src/thunks/postFavorites.test.js
@@ -2,15 +2,29 @@ import { toggleFavorite } from '../Action-creators/toggleFavorite';
 import { errorReceived } from '../Action-creators/errorReceived';
 import { postFavorites } from './postFavorites';
 
-jest.mock();
+jest.mock('../Action-creators/toggleFavorite');
+jest.mock('../Action-creators/errorReceived');
+
 
 describe('postFavorites', () => {
   let mockUrl;
   let mockDispatch;
   let mockUserId;
   let mockMovie;
+  let mockResponse;
 
   beforeEach(() => {
+    mockResponse = {
+      id: 4, 
+      message: 'Movie was added to favorites', 
+      status: 'success'
+    }
+    window.fetch = jest.fn(() => Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+        }
+      )
+    )
     mockUrl = 'http://localhost:3000/api/users/favorites/new';
     mockDispatch = jest.fn();
     mockUserId = 22;
@@ -24,11 +38,39 @@ describe('postFavorites', () => {
     }
   });
 
-  it('should call dispatch with toggleFavorite', () => {
+  it('should call dispatch with toggleFavorite', async () => {
     const thunk = postFavorites(mockUserId, mockMovie);
 
-    thunk(mockDispatch);
+    await thunk(mockDispatch);
 
     expect(mockDispatch).toHaveBeenCalledWith(toggleFavorite(12345));
   });
+  it('should call fetch with the correct params', () => {
+
+    const thunk = postFavorites(mockUserId ,mockMovie);
+
+    thunk(mockDispatch);
+
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+  })
+  it('should toggleFavorite if response is okay', () => {
+    const thunk = postFavorites(mockUserId ,mockMovie); 
+
+    thunk(mockDispatch)
+
+    expect(toggleFavorite).toHaveBeenCalledWith(12345)
+  })
+  it('should throw error if response is not okay', async () => {
+    window.fetch = jest.fn(() => Promise.resolve({
+      ok: false,
+      message: 'ya suck'
+    }))
+
+    const errorMessage = 'Favorite not added, 500 error'
+    const thunk = postFavorites(mockUserId ,mockMovie); 
+
+    await thunk(mockDispatch)
+
+    expect(errorReceived).toHaveBeenCalledWith(errorMessage)
+  })
 });
